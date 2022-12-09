be9c030ff20e05070a57c45176564669e97bd3f1
XavierCoulon <xcoulon.mail@gmail.com>
2022-12-09 17:21:22 +0100

    cors change

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ const app = Express();
 const cors = require("cors");
 const corsOptions = {
   origin: "*",
-  credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 
